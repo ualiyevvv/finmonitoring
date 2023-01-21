0b312cdaa6719cc099e38918ba23bd1a47a8668b
Ayan <xchjjvg07@gmail.com>
2023-01-22 05:28:30 +0600

    connection neo4j, test query, ok

--- database/neo4j.js
+++ database/neo4j.js
@@ -1,5 +1,9 @@
 const neo4j = require('neo4j-driver')
 const driver = neo4j.driver(
-    'neo4j://localhost',    
-    neo4j.auth.basic('neo4j', 'password')
+    'neo4j://localhost:7687',    
+    neo4j.auth.basic('neo4juser', 'root')
 ) 
+const session = driver.session({ defaultAccessMode: neo4j.session.READ });
+
+
+module.exports = session
