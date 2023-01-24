e66fa889053baa8fb3846f8149be021b93f791a1
Ayan <xchjjvg07@gmail.com>
2023-01-25 00:35:59 +0600

    change with dotenv style

--- backend/database/neo4j.js
+++ backend/database/neo4j.js
@@ -1,9 +1,10 @@
 const neo4j = require('neo4j-driver')
 const driver = neo4j.driver(
-    'neo4j://localhost:7687',    
-    neo4j.auth.basic('neo4juser', 'root')
+    `neo4j://${process.env.HOST}:${process.env.NEO_PORT}`,    
+    neo4j.auth.basic(`${process.env.NEO_USER}`, `${process.env.NEO_PASS}`)
 ) 
-const session = driver.session({ defaultAccessMode: neo4j.session.READ });
 
+
+const session = driver.session({ defaultAccessMode: neo4j.session.READ });
 
 module.exports = session
