7c7ccae04739b38946bb0bb881e90f6e226d6ba7
Ayan <xchjjvg07@gmail.com>
2023-02-15 13:58:52 +0600

    graph test working

--- app/client/src/pages/graph_test/GraphTest.js
+++ app/client/src/pages/graph_test/GraphTest.js
@@ -10,6 +10,7 @@ import root from './data/root'
 export default function GraphTest(){
 
     const [graphs, setGraphs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch("/api/data/all")
@@ -17,60 +18,65 @@ export default function GraphTest(){
             .then(result => {
                 let data = result
                 console.log(data)
-                // let newnodes = []
-                // let newrels = []
-                // for (let i=0;i<data.length;i++) {
-                //     console.log(data[0]._fields[i])
-                //     let originalArray = data[0]._fields[i].segments;
-                //     console.log(originalArray)
-                //     originalArray.forEach(function(object) {
-                //         // console.log(object1)
-                //         for (let prop in object) {
-                //             if (!object[prop].labels) {
-                //                 // console.log(object)
-                //                 // indexesRel.push(object[prop].identity.low.toString())
-                //                 newrels.push({
-                //                     "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
-                //                     "startNodeId": "e025a50dbaae7186e51" + object[prop].start.low.toString(),
-                //                     "endNodeId": "e025a50dbaae7186e51" + object[prop].end.low.toString(),
-                //                     "properties": object[prop].properties,
-                //                     "type": object[prop].type,
-                //                 });
-                //             } else {
-                //                 // indexes.push(object[prop].identity.low.toString())
-                //                 newnodes.push({
-                //                     "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
-                //                     "labels": object[prop].labels,
-                //                     "properties": object[prop].properties
-                //                 })
-                //             }
-                //         };
+                let newnodes = []
+                let newrels = []
+                for (let i=0;i<data.length;i++) {
+                    console.log(data[i]._fields[0])
+                    let originalArray = data[i]._fields[0].segments;
+                    console.log(originalArray)
+                    originalArray.forEach(function(object) {
+                        // console.log(object1)
+                        for (let prop in object) {
+                            if (!object[prop].labels) {
+                                // console.log(object)
+                                // indexesRel.push(object[prop].identity.low.toString())
+                                newrels.push({
+                                    "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
+                                    "startNodeId": "e025a50dbaae7186e51" + object[prop].start.low.toString(),
+                                    "endNodeId": "e025a50dbaae7186e51" + object[prop].end.low.toString(),
+                                    "properties": object[prop].properties,
+                                    "type": object[prop].type,
+                                });
+                            } else {
+                                // indexes.push(object[prop].identity.low.toString())
+                                newnodes.push({
+                                    "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
+                                    "labels": object[prop].labels,
+                                    "properties": object[prop].properties
+                                })
+                            }
+                        };
 
-                //     });
-                // }
+                    });
+                }
 
-                // var resultData = {
-                //     "nodes": newnodes,
-                //     "relationships": newrels
-                // }
+                var resultData = {
+                    "nodes": newnodes,
+                    "relationships": newrels
+                }
 
-                // setGraphs(resultData);
+                setGraphs(resultData);
+                setIsLoading(false);
+
             });
 
     }, []);
 
     return (
         <Main>
-            <ReactGraph
-                initialState={graphs}
-                nodes={nodes}
-                relationships={relationships}
-                width="100%"
-                height="100%"
-                hasLegends
-                hasInspector
-                hasTruncatedFields
-            />
+            { isLoading ? '' : 
+                <ReactGraph
+                    initialState={graphs}
+                    nodes={nodes}
+                    relationships={relationships}
+                    width="100%"
+                    height="100%"
+                    hasLegends
+                    hasInspector
+                    hasTruncatedFields
+                />
+            }
+            
         </Main>
     );
 }
