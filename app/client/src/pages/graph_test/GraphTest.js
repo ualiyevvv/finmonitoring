59071602b5260752be0b56e95c3f0106cd160b4d
unknown <alar.akilbekov99@gmail.com>
2023-02-15 13:23:51 +0600

    stuppppid

--- app/client/src/pages/graph_test/GraphTest.js
+++ app/client/src/pages/graph_test/GraphTest.js
@@ -7,56 +7,55 @@ import relationships from './data/relationships'
 import root from './data/root'
 
 
+export default function GraphTest(){
 
-const [graphs, setGraphs] = useState([]);
+    const [graphs, setGraphs] = useState([]);
 
-useEffect(() => {
-    fetch("http://localhost:3000/api/data/all")
-        .then(response => response.json())
-        .then(result => {
-            let data = result
-            let newnodes = []
-            let newrels = []
-            console.log(data[0]._fields[0])
-            let originalArray = data[0]._fields[0].segments;
-            console.log(originalArray)
-            originalArray.forEach(function(object) {
-                // console.log(object1)
-                for (let prop in object) {
-                    if (!object[prop].labels) {
-                        // console.log(object)
-                        // indexesRel.push(object[prop].identity.low.toString())
-                        newrels.push({
-                            "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
-                            "startNodeId": "e025a50dbaae7186e51" + object[prop].start.low.toString(),
-                            "endNodeId": "e025a50dbaae7186e51" + object[prop].end.low.toString(),
-                            "properties": object[prop].properties,
-                            "type": object[prop].type,
-                        });
-                    } else {
-                        // indexes.push(object[prop].identity.low.toString())
-                        newnodes.push({
-                            "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
-                            "labels": object[prop].labels,
-                            "properties": object[prop].properties
-                        })
-                    }
-                };
-            
+    useEffect(() => {
+        fetch("http://localhost:3000/api/data/all")
+            .then(response => response.json())
+            .then(result => {
+                let data = result
+                let newnodes = []
+                let newrels = []
+                console.log(data[0]._fields[0])
+                let originalArray = data[0]._fields[0].segments;
+                console.log(originalArray)
+                originalArray.forEach(function(object) {
+                    // console.log(object1)
+                    for (let prop in object) {
+                        if (!object[prop].labels) {
+                            // console.log(object)
+                            // indexesRel.push(object[prop].identity.low.toString())
+                            newrels.push({
+                                "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
+                                "startNodeId": "e025a50dbaae7186e51" + object[prop].start.low.toString(),
+                                "endNodeId": "e025a50dbaae7186e51" + object[prop].end.low.toString(),
+                                "properties": object[prop].properties,
+                                "type": object[prop].type,
+                            });
+                        } else {
+                            // indexes.push(object[prop].identity.low.toString())
+                            newnodes.push({
+                                "id": "e025a50dbaae7186e51" + object[prop].identity.low.toString(),
+                                "labels": object[prop].labels,
+                                "properties": object[prop].properties
+                            })
+                        }
+                    };
+
+                });
+
+                var resultData = {
+                    "nodes": newnodes,
+                    "relationships": newrels
+                }
+
+                setGraphs(resultData);
             });
 
-            var resultData = {
-                "nodes": newnodes,
-                "relationships": newrels 
-            }
-                    
-            setGraphs(resultData);
-        });
-    
-}, []);
+    }, []);
 
-
-export default function GraphTest(){
     return (
         <Main>
             <ReactGraph
