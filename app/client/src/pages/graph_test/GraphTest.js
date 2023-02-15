911f3a80391113e93122a2a44ec2f968c8c55c12
unknown <alar.akilbekov99@gmail.com>
2023-02-15 07:09:28 +0600

    react graph test

--- app/client/src/pages/graph_test/GraphTest.js
+++ app/client/src/pages/graph_test/GraphTest.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Main from "../main/Main";
+import ReactGraph from "react-graph";
+
+import nodes from './data/nodes'
+import relationships from './data/relationships'
+import root from './data/root'
 
 export default function GraphTest(){
     return (
-        <Main>
+        <>
             <h1>[Graph Test]</h1>
-        </Main>
+            <ReactGraph
+                initialState={root}
+                nodes={nodes}
+                relationships={relationships}
+                width="100%"
+                height="100%"
+                hasLegends
+                hasInspector
+                hasTruncatedFields
+            />
+        </>
     );
 }
